f2d1c85043e6003b7abd2f67738abde4f6ef017c
GAB <69873622+gabsysdev@users.noreply.github.com>
2023-08-30 14:54:57 -0300

    Create getAWSCognitoTokenFromApigeePolicy.sh

---
+++ getAWSCognitoTokenFromApigeePolicy.sh
@@ -0,0 +1,14 @@
+#Gets AWS Token (Cognito) from an Apigee Policy
+#Saves the full response from the sent curl command in the file "token.fullResponse"
+#Saves the "AccessToken" argument from the response as the file "token.accessToken"
+RESPONSE=$(curl --location 'https://cognito-idp.us-east-1.amazonaws.com/' \
+--header 'X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth' \
+--header 'Content-Type: application/x-amz-json-1.1' \
+--data '{
+   "AuthParameters" : {
+      "USERNAME" : "paciente1",
+      "PASSWORD" : "paciente1"
+   },  
+   "AuthFlow" : "USER_PASSWORD_AUTH",
+   "ClientId" : "42q4i0agn6sq1eucor7g6tgbv0"
+}') ; jq '.' <<< $RESPONSE > token.fullResponse; TOKEN=$(echo $RESPONSE | jq '.[] | .AccessToken' | head -1) ; curl -i https://34.160.224.233.nip.io/oauth/token -X POST -H "Authorization: Bearer $TOKEN" ; echo $TOKEN > token.accessToken
