ec023086d451abbb432733294292978de44899cb
GAB <69873622+gabsysdev@users.noreply.github.com>
2023-08-30 03:08:42 -0300

    Update GCP_GetProjectsOnFolder.sh
    Added alternative to execute without the need of temporary files.

--- GCP_GetProjectsOnFolder.sh
+++ GCP_GetProjectsOnFolder.sh
@@ -1,5 +1,18 @@
 #GCP
 #Show all the projects inside a folder (asks for folder ID)
+
+#No tmp files created:
+read -p 'Ingrese ID de carpeta:' folder
+
+echo "Searching for projects on this folder..."
+
+gcloud asset search-all-resources --scope='folders/$folder' | grep 'project: projects' | sort -u | sed 's/project: projects//g' | \
+while read project; do
+        echo "ProjectNumber: $project" ; gcloud projects describe $project | grep 'name: \|projectId: '
+done
+
+
+#Alternative way, tmp files are created:
 read -p 'Ingrese ID de carpeta:' folder
 
 echo "Searching for projects on this folder..."
